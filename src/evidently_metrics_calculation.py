f89436750151b783986323bb132504395cc02e62
Pedro Chitarra <pedrochitarra@gmail.com>
2024-08-17 20:24:04 -0300

    [METRICS] Evidently code reorganized

--- src/evidently_metrics_calculation.py
+++ src/evidently_metrics_calculation.py
@@ -1,15 +1,12 @@
 """Script to simulate model monitoring with Evidently in the test set and
 store the metrics in a PostgreSQL database."""
 # pylint: disable=logging-fstring-interpolation
-import datetime
 import logging
 import pickle
-import random
-import time
 
 import pandas as pd
+from tqdm import tqdm
 import psycopg
-import yaml
 from evidently.metrics import (ClassificationClassBalance,
                                ClassificationConfusionMatrix,
                                ClassificationDummyMetric,
@@ -19,60 +16,16 @@ from evidently.metrics import (ClassificationClassBalance,
 from evidently.report import Report
 from prefect import flow, task
 
-# TODO: Use Grafana Cloud to visualize the metrics and dashboards. Learn how to
-# use Grafana Cloud to visualize the metrics and dashboards.
-
-# TODO: Organize this code in a more modular way!
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
-
-SEND_TIMEOUT = 10
-rand = random.Random()
-
-create_table_statement = """
-drop table if exists heart_metrics;
-create table heart_metrics(
-    batch_number integer,
-    precision_ float,
-    recall float,
-    f1 float,
-    accuracy float
-)
-"""
-
-test_data = pd.read_parquet('data/interim/heart_test.parquet')
-with open('model.pkl', 'rb') as f_in:
-    model = pickle.load(f_in)
-
-reference_data = pd.read_parquet('data/processed/heart_train_cleaned.parquet')
-
-# Load constants/categorical_features from params.yaml
-with open("params.yaml", encoding="utf-8") as file:
-    dvc_params = yaml.safe_load(file)
-
-cat_features = dvc_params["categorical_features"]
-num_features = list(set(reference_data.columns) -
-                    set(cat_features) - {'HadHeartAttack'})
-
-report = Report(metrics=[
-    ClassificationQualityMetric(),
-    ClassificationClassBalance(),
-    ConflictTargetMetric(),
-    ConflictPredictionMetric(),
-    ClassificationConfusionMatrix(),
-    ClassificationQualityByClass(),
-    ClassificationDummyMetric(),
-])
-
-conn_str = "host=localhost port=5432 user=postgres password=example"
-table_str = ("host=localhost port=5432 dbname=test "
-             "user=postgres password=example")
-
 
 @task
-def prep_db():
-    """Prepare the database for the metrics."""
+def prep_db(conn_str: str, table_str: str, create_table_statement: str):
+    """Prepare the database for the metrics.
+
+    Args:
+        conn_str (str): Connection string to create the database.
+        table_str (str): Connection string to connect to the database.
+        create_table_statement (str): SQL statement to create the table.
+    """
     conn = psycopg.connect(conn_str)
     cur = conn.cursor()
     res = cur.execute("SELECT 1 FROM pg_database WHERE datname='test'")
@@ -87,9 +40,23 @@ def prep_db():
 
 
 @task
-def calculate_metrics_postgresql(cur, i):
-    """Calculate the metrics for raw data and store them in the database."""
-    current_data = test_data.iloc[i:i + 10]
+def calculate_metrics_postgresql(cur: psycopg.cursor,
+                                 i: int, model: object, report: object,
+                                 reference_data: pd.DataFrame,
+                                 test_data: pd.DataFrame,
+                                 batch_size: int = 50):
+    """Calculate the metrics for raw data and store them in the database.
+
+    Args:
+        cur (psycopg.cursor): Cursor to the database.
+        i (int): Index to start the batch.
+        model (object): Model to predict the data.
+        report (object): Report object to calculate the metrics.
+        reference_data (pd.DataFrame): Reference data to calculate the metrics.
+        test_data (pd.DataFrame): Raw data to calculate the metrics.
+        batch_size (int): Size of the batch to calculate the metrics.
+    """
+    current_data = test_data.iloc[i:i + batch_size]
 
     if current_data.isna().sum().sum() > 0:
         current_data.fillna(0, inplace=True)
@@ -116,7 +83,7 @@ def calculate_metrics_postgresql(cur, i):
     f1 = result['metrics'][0]['result']['current']['f1']
     accuracy = result['metrics'][0]['result']['current']['accuracy']
 
-    batch_number = int(i / 10)
+    batch_number = int(i / batch_size)
 
     cur.execute(
         """insert into heart_metrics(batch_number, precision_,
@@ -127,25 +94,59 @@ def calculate_metrics_postgresql(cur, i):
 
 
 @flow
-def batch_monitoring_backfill():
+def batch_monitoring_backfill(table_str: str, test_data: pd.DataFrame,
+                              model: object, report: object,
+                              reference_data: pd.DataFrame,
+                              batch_size: int = 50):
     """Flow to calculate the metrics for the test data and store them in the
-    database."""
-    prep_db()
-    last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
+    database. The metrics are calculated for every batch of 10 rows."""
     conn = psycopg.connect(table_str, autocommit=True)
     cur = conn.cursor()
-    for i in range(0, len(test_data), 10):
-        calculate_metrics_postgresql(cur, i)
-        new_send = datetime.datetime.now()
-        seconds_elapsed = (new_send - last_send).total_seconds()
-        if seconds_elapsed < SEND_TIMEOUT:
-            time.sleep(SEND_TIMEOUT - seconds_elapsed)
-        while last_send < new_send:
-            last_send = last_send + datetime.timedelta(seconds=2)
+    for i in tqdm(range(0, len(test_data), batch_size),
+                  desc="Calculating metrics"):
+        calculate_metrics_postgresql(cur, i, model, report,
+                                     reference_data, test_data, batch_size)
         logging.info("data sent")
 
 
-if __name__ == '__main__':
+def main():
+    """Main function to run the flow."""
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+
+    batch_size = 500
+    create_table_statement = """
+    drop table if exists heart_metrics;
+    create table heart_metrics(
+        batch_number integer,
+        precision_ float,
+        recall float,
+        f1 float,
+        accuracy float
+    )
+    """
+
+    test_data = pd.read_parquet('data/interim/heart_test.parquet')
+    with open('model.pkl', 'rb') as f_in:
+        model = pickle.load(f_in)
+
+    reference_data = pd.read_parquet(
+        'data/processed/heart_train_cleaned.parquet')
+
+    report = Report(metrics=[
+        ClassificationQualityMetric(),
+        ClassificationClassBalance(),
+        ConflictTargetMetric(),
+        ConflictPredictionMetric(),
+        ClassificationConfusionMatrix(),
+        ClassificationQualityByClass(),
+        ClassificationDummyMetric(),
+    ])
+
+    conn_str = "host=localhost port=5432 user=postgres password=example"
+    table_str = ("host=localhost port=5432 dbname=test "
+                 "user=postgres password=example")
+
     reference_data_preds = model.predict(reference_data.drop(columns=[
         "HadHeartAttack"]))
     reference_data['prediction'] = reference_data_preds
@@ -157,4 +158,10 @@ if __name__ == '__main__':
     reference_data['target'] = reference_data['target'].replace(
         {"No": 0, "Yes": 1})
 
-    batch_monitoring_backfill()
+    prep_db(conn_str, table_str, create_table_statement)
+    batch_monitoring_backfill(table_str, test_data,
+                              model, report, reference_data, batch_size)
+
+
+if __name__ == "__main__":
+    main()
